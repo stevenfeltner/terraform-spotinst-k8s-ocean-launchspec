ea91e93ced52c3847ad30bc7cb6eba24b33f05b6
Steven Feltner <steven.feltner@netapp.com>
2021-06-23 20:39:03 -0700

    Add block device mapping

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = ">= 0.13.0"
   required_providers {
     spotinst = {
       source = "spotinst/spotinst"
@@ -17,13 +18,33 @@ resource "spotinst_ocean_aws_launch_spec" "nodegroup" {
   ocean_id = var.ocean_id
   name = var.name
 
-  user_data               = var.user_data
-  image_id                = var.ami_id
-  iam_instance_profile    = var.worker_instance_profile_arn
-  security_groups         = var.security_groups
-  subnet_ids              = var.subnet_ids
-  instance_types          = var.instance_types
-  root_volume_size        = var.root_volume_size
+  user_data                   = var.user_data
+  image_id                    = var.ami_id
+  iam_instance_profile        = var.worker_instance_profile_arn
+  security_groups             = var.security_groups
+  subnet_ids                  = var.subnet_ids
+  instance_types              = var.instance_types
+  root_volume_size            = var.root_volume_size
+  associate_public_ip_address = var.associate_public_ip_address
+  restrict_scale_down         = var.restrict_scale_down
+
+  # Required tags
+  tags {
+    key = "Name"
+    value = "${var.cluster_name}-ocean-cluster-node"
+  }
+  tags {
+    key = "kubernetes.io/cluster/${var.cluster_name}"
+    value = "owned"
+  }
+
+  dynamic tags {
+    for_each = var.tags == null ? [] : var.tags
+    content {
+      key = tags.value["key"]
+      value = tags.value["value"]
+    }
+  }
 
   dynamic labels {
     for_each = var.labels == null ? [] : var.labels
@@ -42,28 +63,43 @@ resource "spotinst_ocean_aws_launch_spec" "nodegroup" {
     }
   }
 
-  resource_limits {
-    max_instance_count    = var.max_instance_count
-  }
-
-  tags {
-    key = "Name"
-    value = "${var.cluster_name}-ocean-cluster-node"
-  }
-  tags {
-    key = "kubernetes.io/cluster/${var.cluster_name}"
-    value = "owned"
-  }
-
-  dynamic tags {
-    for_each = var.tags == null ? [] : var.tags
-    content {
-      key = tags.value["key"]
-      value = tags.value["value"]
+  ## Block Device Mappings ##
+  block_device_mappings {
+    device_name                 = var.device_name
+    ebs {
+      delete_on_termination     = var.delete_on_termination
+      encrypted                 = var.encrypted
+      iops                      = var.iops
+      kms_key_id                = var.kms_key_id
+      snapshot_id               = var.snapshot_id
+      volume_type               = var.volume_type
+      volume_size               = var.volume_size
+      throughput                = var.throughput
+      dynamic_volume_size {
+        base_size               = var.base_size
+        resource                = var.resource
+        size_per_resource_unit  = var.size_per_resource_unit
+      }
+      no_device                 = var.no_device
     }
   }
 
+  autoscale_headrooms {
+    cpu_per_unit                = var.cpu_per_unit
+    memory_per_unit             = var.memory_per_unit
+    gpu_per_unit                = var.gpu_per_unit
+    num_of_units                = var.num_of_units
+  }
+
+  resource_limits {
+    max_instance_count          = var.max_instance_count
+  }
+
   strategy {
-    spot_percentage = var.spot_percentage
+    spot_percentage             = var.spot_percentage
+  }
+
+  create_actions {
+    initial_nodes               = var.initial_nodes
   }
 }
--- variables.tf
+++ variables.tf
@@ -63,6 +63,16 @@ variable "root_volume_size" {
   default     = 30
   description = "Size of root volume"
 }
+variable "associate_public_ip_address" {
+  type        = bool
+  default     = null
+  description = "Configure public IP address allocation."
+}
+variable "restrict_scale_down" {
+  type        = bool
+  default     = null
+  description = "When set to True, nodes will be treated as if all pods running have the restrict-scale-down label. Therefore, Ocean will not scale nodes down unless empty."
+}
 variable "spot_percentage" {
   type        = number
   default     = 100
@@ -95,3 +105,104 @@ variable "tags" {
 }
 
 
+## Block Device Mappings ##
+variable "device_name" {
+  type        = string
+  default     = ""
+  description = "Set device name. (Example: /dev/xvda1)."
+}
+variable "delete_on_termination" {
+  type        = string
+  default     = null
+  description = "Flag to delete the EBS on instance termination."
+}
+variable "encrypted" {
+  type        = bool
+  default     = null
+  description = "Enables EBS encryption on the volume."
+}
+variable "iops" {
+  type        = string
+  default     = null
+  description = "(Required for requests to create io1 volumes; it is not used in requests to create gp2, st1, sc1, or standard volumes) Int. The number of I/O operations per second (IOPS) that the volume supports."
+}
+variable "kms_key_id" {
+  type        = string
+  default     = null
+  description = "Identifier (key ID, key alias, ID ARN, or alias ARN) for a customer managed CMK under which the EBS volume is encrypted."
+}
+variable "snapshot_id" {
+  type        = string
+  default     = null
+  description = "The Snapshot ID to mount by."
+}
+variable "volume_type" {
+  type        = string
+  default     = null
+  description = "The type of the volume. (Example: gp2)."
+}
+variable "volume_size" {
+  type        = number
+  default     = null
+  description = "The size, in GB of the volume."
+}
+variable "throughput" {
+  type        = number
+  default     = null
+  description = "The amount of data transferred to or from a storage device per second, you can use this param just in a case that volume_type = gp3."
+}
+###################
+
+## Dynamic Volume Size ##
+variable "base_size" {
+  type        = number
+  default     = 30
+  description = "Int. Initial size for volume. (Example: 50)"
+}
+variable "resource" {
+  type        = string
+  default     = "CPU"
+  description = "Resource type to increase volume size dynamically by. (Valid values: CPU)"
+}
+variable "size_per_resource_unit" {
+  type        = number
+  default     = 20
+  description = "Int. Additional size (in GB) per resource unit. (Example: baseSize=50, sizePerResourceUnit=20, and instance with 2 CPU is launched; its total disk size will be: 90GB)"
+}
+variable "no_device" {
+  type        = string
+  default     = null
+  description = "String. Suppresses the specified device included in the block device mapping of the AMI."
+}
+##################
+
+## Headroom ##
+variable "cpu_per_unit" {
+  type        = number
+  default     = null
+  description = "Optionally configure the number of CPUs to allocate for each headroom unit. CPUs are denoted in millicores, where 1000 millicores = 1 vCPU."
+}
+variable "memory_per_unit" {
+  type        = number
+  default     = null
+  description = "Optionally configure the amount of memory (MiB) to allocate for each headroom unit."
+}
+variable "gpu_per_unit" {
+  type        = number
+  default     = null
+  description = "Optionally configure the number of GPUS to allocate for each headroom unit."
+}
+variable "num_of_units" {
+  type        = number
+  default     = 0
+  description = "The number of units to retain as headroom, where each unit has the defined headroom CPU, memory and GPU."
+}
+##################
+
+## Create Actions ##
+variable "initial_nodes" {
+  type        = number
+  default     = 0
+  description = "When set to an integer greater than 0, a corresponding amount of nodes will be launched from the created virtual node group."
+}
+##################
